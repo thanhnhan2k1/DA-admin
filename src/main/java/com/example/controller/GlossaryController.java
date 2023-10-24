e28cf0a2a1db0a0a9440b821fc660a163b9e7729
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-10-25 01:30:19 +0700

    Update GlossaryController.java

--- src/main/java/com/example/controller/GlossaryController.java
+++ src/main/java/com/example/controller/GlossaryController.java
@@ -38,9 +38,8 @@ import com.example.model.Glossary;
 @RequestMapping("/glossary")
 public class GlossaryController {
 	private RestTemplate rest = new RestTemplate();
-	private String url = "http://localhost:8082";
-
-	//private String url="https://da-server2-production.up.railway.app";
+	//private String url="http://localhost:8082";
+	private String url="https://server-production-004b.up.railway.app";
 	@GetMapping
 	private String get(@RequestParam(name = "key", defaultValue = "", required = false) String key, HttpSession session,
 			Model model) {
