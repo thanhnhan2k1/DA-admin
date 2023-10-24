c90184d8f2df1c2e5a02004ba14ac39ade7f0c2c
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-10-25 01:28:40 +0700

    Update BlogController.java

--- src/main/java/com/example/controller/BlogController.java
+++ src/main/java/com/example/controller/BlogController.java
@@ -36,8 +36,8 @@ import com.example.model.Wood;
 public class BlogController {
 	private RestTemplate rest=new RestTemplate();
 	
-	private String url="http://localhost:8082";
-	//private String url="https://da-server2-production.up.railway.app";
+	//private String url="http://localhost:8082";
+	private String url="https://server-production-004b.up.railway.app";
 	@GetMapping
 	private String getListBlog(HttpSession session,@RequestParam(name="status", required = false, defaultValue = "publish")String status) {
 		List<Blog>listBlog=new ArrayList<>();
